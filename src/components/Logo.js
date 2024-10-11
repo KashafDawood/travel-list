8bf939ad9b6e6c3e7e15be23b3be72d5ffde9d25
kashafdawood <kashaf.dawood@gmail.com>
2024-10-11 08:14:32 +0500

    deploy on github

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <h1>
+      <span role="img" aria-label="palm tree">
+        🌴
+      </span>{" "}
+      For Away{" "}
+      <span role="img" aria-label="breif case">
+        👜
+      </span>
+    </h1>
+  );
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        {" "}
+        <em>
+          Start adding some items to your packing list{" "}
+          <span role="img" aria-label="rocket">
+            {" "}
+            🚀{" "}
+          </span>
+        </em>{" "}
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 Your have ${numItems} items on your list, and you have already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
